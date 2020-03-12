3c1a433de3109a224c64cf5870cb3ec2469f45ba
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-03-12 21:08:51 +0900

    React-Router 설치 및 프로젝트 구성
    SPA가 아닌 다수의 페이지를 운영하기 위한 React-Router 설치와 프로젝트 구성.
    Hello React-Router 출력.
    - next: 리액트 라우터 사용하기

---
+++ src/shared/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+// 앱 컴포넌트: 어떤 주소로 왔을 때 무엇을 보여줄지 정의
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello React-Router
+      </div>
+    );
+  }
+}
+
+export default App;
