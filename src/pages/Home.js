84ff0ee0f83b3735cea0dbe26092beb08e1a8b2a
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-04-13 16:12:30 +0900

    Home 컴포넌트 추가(텍스트 필드, 버튼)
    material-ui 설치 (yarn)
    - home화면에 이름, 이메일 입력받을 텍스트필드와 제출하기 버튼 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import TextField from '@material-ui/core/TextField';
-// import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const Home = () => {
     return (
@@ -14,10 +14,22 @@ const Home = () => {
                     <img class="Home-introduce" src="home_introduce.png"/>
                 </div>
                 <div class="Home-body">
-                    <img class="Home-input-name" src="input_box.png"/>
-                    <img class="Home-input-email" src="input_box.png"/>
-                    {/* <img class="Home-policy" src=""/> //개인정보 정책 */}
-                    <img class="Home-btn-subscribe" src="btn_subscribe.png"/>
+                    {/* 이름 받아올 textfield
+                     <img id="input_name" class="Home-input-name" src="input_box.png"/> */}
+                    <TextField id="input_name" label="Outlined" variant="outlined"/>
+
+                    {/* 이메일 받아올 textfield
+                    <img class="Home-input-email" src="input_box.png"/> */}
+                    <TextField id="input_email" label="Outlined" variant="outlined"/>
+
+                    {/* 개인정보 정책에 연결시켜주는 버튼
+                    <img class="Home-policy" src=""/> */}
+
+
+                    {/* 제출 버튼
+                    <img class="Home-btn-subscribe" src="btn_subscribe.png"/> */}
+                    <Button id="btn_subscribe" variant="outlined">제출하기</Button>
+
                     <img class="Line-border" src="line_border.png"/>
                 </div>
             </body>
