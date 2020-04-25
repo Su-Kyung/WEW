1f432e8c2b4a7772f59dc8b5169bf645d78d3ef1
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-04-25 19:02:53 +0900

    [UPDATE] header style
    - menu button (image)
    - layout

--- src/shared/App.js
+++ src/shared/App.js
@@ -11,12 +11,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div>
+        <header class="App-header">
           <img class="App-header-logo" src="/logo_indigo.png"/>
 
           {/* <img class="Button-menu" src="/btn_menu.png"/> */}
-          <Button id="btn_menu" >메뉴</Button>
-        </div>
+          <Button id="btn_menu" class="Button-menu">
+            <img src="/btn_menu.png" width="71.2px" height="auto" text-align="center"/>
+          </Button>
+        </header>
         <Route exact path="/" component={Home}/>
         <Route path="/about" component={About}/>
         <Route path="/feedback" component={Feedback}/>
