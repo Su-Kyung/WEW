17621c90ed5469520baed79bcb0e7ebd8df9a03b
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-05-17 22:04:29 +0900

    update feedback button style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
+    textfield: {
       '& > *': {
         margin: theme.spacing(0.7),
         width: '424px',
@@ -14,8 +14,14 @@ const useStyles = makeStyles((theme) => ({
         borderRadius: 10,
       },
     },
+    button: {
+        '& > *': {
+          titleColor: '#fff',
+        },
+      },
   }));
-  
+
+
 
 const Home = () => {
     const classes = useStyles();
@@ -31,18 +37,19 @@ const Home = () => {
                 </div>
                 <div class="Home-body">
                     <Container maxWidth="sm">
-                    <form className={classes.root} noValidate autoComplete="off">
-                    {/* 이름 받아올 textfield
-                     <img id="input_name" class="Home-input-name" src="input_box.png"/> */}
+                    <form className={classes.textfield} noValidate autoComplete="off">
+
                     <TextField
                         id="input_name"
                         placeholder="이름을 입력하세요."
                         variant="outlined"
                         fullWidth="true"/>
 
-                    {/* 이메일 받아올 textfield
-                    <img class="Home-input-email" src="input_box.png"/> */}
-                    <TextField id="input_email" placeholder="이메일을 입력하세요." variant="outlined" fullWidth="true"/>
+                    <TextField
+                        id="input_email"
+                        placeholder="이메일을 입력하세요."
+                        variant="outlined"
+                        fullWidth="true"/>
 
                     {/* 개인정보 정책에 연결시켜주는 버튼
                     <img class="Home-policy" src=""/> */}
@@ -52,7 +59,12 @@ const Home = () => {
                     </form>
                     {/* 제출 버튼
                     <img class="Home-btn-subscribe" src="btn_subscribe.png"/> */}
-                    <Button id="btn_subscribe" variant="outlined" >구독하기</Button>
+                    <Button
+                        id="btn_subscribe"
+                        variant="outlined"
+                        titleColor="#fff"
+                        className={classes.button}
+                        >구독하기</Button>
                     </Container>
                     <img class="Line-border" src="line_border.png"/>
                 </div>
