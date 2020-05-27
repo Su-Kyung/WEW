f3180a684fde72184e36609e2196088121668046
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-05-28 03:19:57 +0900

    feedback.js 본문 컴포넌트 추가

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
     textfield: {
@@ -32,28 +33,32 @@ const Feedback = () => {
                 <img class="Feedback-waiting" src="feedback_waiting.png"/>
             </div>
             <div class="Feedback-body">
-                <Container maxWidth="sm">
-                    {/* 피드백 제출하기 버튼
-                    <img class="Feedback-btn-submit" src="btn_submit.png"/> */}
+                <Grid 
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center">
                     <Button id="btn_submit" variant="outlined">제출하기</Button>
+                    <Grid item xs={12}>
+                    <img src="feedback_email.png" height="27px"/>
+                    <TextField
+                        id="feedback_email"
+                        placeholder="회신받을 이메일 주소를 입력하세요."
+                        variant="outlined"
+                        className={classes.textfield}/>
+                    </Grid>
+                    <Grid item xs={12}>
+                    <img src="feedback_content.png" height="27px"/>
+                    <TextField
+                        id="feedback_contents"
+                        multiline
+                        variant="outlined"
+                        placeholder="내용을 입력하세요."
+                        rows={10}
+                        className={classes.textfield} />
+                    </Grid>
                     
-                    <form className={classes.textfield} noValidate autoComplete="off">
-                        {/* 피드백 작성
-                        <img class="Feedback-input" src="feedback_email.png"/>
-                        <img class="Home-input-email" src="input_box.png"/> */}
-                        <TextField
-                            id="feedback_email"
-                            placeholder="회신받을 이메일 주소를 입력하세요."
-                            variant="outlined"/>
-
-                        <TextField
-                            id="feedback_contents"
-                            multiline
-                            placeholder="내용을 입력하세요."
-                            rows={10} />
-
-                    </form>
-                </Container>
+                </Grid>
 
             </div>
         </div>
