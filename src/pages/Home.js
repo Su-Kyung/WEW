5ba7f6101e9fe55b537110791401eb07dd04361b
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-05-28 02:34:19 +0900

    Update Home.js components
    그리드 활용하여 정렬!
    가운데 정렬, 화면 에 대해 flexible하게 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,16 +4,14 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
-    // container: {
-    //     '& > *': {
-    //         FlexDirection="column",
-    //         flex="1",
-    //         alignItems="center",
-    //         color="red",
-    //     },
-    //   },
+    root: {
+        '& > *': {
+        flexGrow: 1,
+        },
+    },
     textfield: {
       '& > *': {
         margin: theme.spacing(0.7),
@@ -21,6 +19,7 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: '#fff',
         overflow: 'hidden',
         borderRadius: 10,
+        margin: '9px',
       },
     },
     button: {
@@ -47,31 +46,38 @@ const Home = () => {
                 <div>
                     <img class="Home-introduce" src="home_introduce.png"/>
                 </div>
-                <div class="Home-body">
-                    <Container maxWidth="sm" text-align="center">
-                    <form className={classes.textfield} noValidate autoComplete="off">
-
-                    <TextField
-                        id="input_name"
-                        placeholder="이름을 입력하세요."
-                        variant="outlined"
-                        fullWidth="true"/>
-
-                    <TextField
-                        id="input_email"
-                        placeholder="이메일을 입력하세요."
-                        variant="outlined"
-                        fullWidth="true"/>
-
-                    {/* 개인정보 정책에 연결시켜주는 버튼
-                    <img class="Home-policy" src=""/> */}
-                    </form>
-
-                    <Button><img src="btn_subscribe.png" width="157.9px"></img></Button>
+                {/* <div class="Home-body"> */}
+                <div className={classes.root} class="Home-body">
+                    {/* <Container maxWidth="sm" alighItems="center"> */}
+                    <Grid 
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="center">
+                        <form className={classes.textfield} noValidate autoComplete="off">
+                        <Grid item xs={12}>    
+                            <TextField
+                                id="input_name"
+                                placeholder="이름을 입력하세요."
+                                variant="outlined"
+                                fullWidth="true"/>
+                        </Grid>
+                        <Grid item xs={12}> 
+                            <TextField
+                                id="input_email"
+                                placeholder="이메일을 입력하세요."
+                                variant="outlined"
+                                fullWidth="true"/>
+                        </Grid>
+                            {/* 개인정보 정책에 연결시켜주는 버튼
+                            <img class="Home-policy" src=""/> */}
+                            </form>
+                        <Grid item xs={12}>
+                            <Button><img src="btn_subscribe.png" width="157.9px"></img></Button>
+                        </Grid>
+                    </Grid>
                     
-                    
-                    
-                    </Container>
+                    {/* </Container> */}
                     <img class="Line-border" src="line_border.png"/>
                 </div>
             </body>
