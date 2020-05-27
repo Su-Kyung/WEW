48ce5f28247f7a134a1fe5639f8939522ffe6723
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-05-28 04:28:11 +0900

    Update Feedback.js
    그리드,,, 컴포넌트 확실히 알고 쓰자
    자꾸 조금씩 안맞음,,,

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -33,33 +33,63 @@ const Feedback = () => {
                 <img class="Feedback-waiting" src="feedback_waiting.png"/>
             </div>
             <div class="Feedback-body">
+                <Container maxWidth="sm">
                 <Grid 
                     container
                     direction="column"
                     justify="center"
-                    alignItems="center">
-                    <Button id="btn_submit" variant="outlined">제출하기</Button>
-                    <Grid item xs={12}>
-                    <img src="feedback_email.png" height="27px"/>
-                    <TextField
-                        id="feedback_email"
-                        placeholder="회신받을 이메일 주소를 입력하세요."
-                        variant="outlined"
-                        className={classes.textfield}/>
-                    </Grid>
-                    <Grid item xs={12}>
-                    <img src="feedback_content.png" height="27px"/>
-                    <TextField
-                        id="feedback_contents"
-                        multiline
-                        variant="outlined"
-                        placeholder="내용을 입력하세요."
-                        rows={10}
-                        className={classes.textfield} />
-                    </Grid>
-                    
-                </Grid>
+                    alignItems="center"
+                    spacing={2}
+                    >
 
+                    <Grid item container justify="flex-end">
+                        <Button id="btn_submit">
+                            <img src="btn_submit.png" width="157.9px"></img>
+                        </Button>
+                    </Grid>
+
+                    <Grid item container spacing={1} flexGrow={1}>
+                        <Grid
+                        item xs={12}
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        >
+                          
+                            <img src="feedback_email.png" height="27px"/>
+                         
+                         
+                            <TextField
+                                id="feedback_email"
+                                placeholder="회신받을 이메일 주소를 입력하세요."
+                                variant="outlined"
+                                className={classes.textfield}/>
+                                
+                           
+                          
+                        </Grid>
+
+
+                        <Grid
+                        item xs={12}
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="flex-start">
+                        <img src="feedback_content.png" height="27px"/>
+                        <TextField
+                            id="feedback_contents"
+                            multiline
+                            variant="outlined"
+                            placeholder="내용을 입력하세요."
+                            rows={10}
+                            className={classes.textfield} />
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+                </Container>
             </div>
         </div>
     )
