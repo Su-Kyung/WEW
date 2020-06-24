ed6140a9ebdbeb91f3ecc1c25e25332ef77d5fb3
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-06-24 19:30:50 +0900

    Modal Example test

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -8,6 +8,24 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 
+import SimpleModal from '@material-ui/core/Modal';
+import Modal from '@material-ui/core/Modal';
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
 const useStyles = makeStyles((theme) => ({
   toolbar: {
     // borderBottom: `1px solid ${theme.palette.divider}`,
@@ -32,10 +50,41 @@ const useStyles = makeStyles((theme) => ({
   fakeIcon: {
     color: '#fff',
   },
+
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }));
 
 export default function Header() {
   const classes = useStyles();
+
+
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Text in a modal</h2>
+      <p id="simple-modal-description">
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </p>
+      <SimpleModal />
+    </div>
+  );
 
   return (
     <React.Fragment>
@@ -53,10 +102,19 @@ export default function Header() {
         >
           <img src="/logo_indigo.png" width="188px"></img>
         </Typography>
-        <IconButton>
-          <MenuIcon style={{ fontSize: 80 }} className={classes.menuIcon} />
+        <IconButton type="button" onClick={handleOpen}>
+          <MenuIcon style={{ fontSize: 80 }} className={classes.menuIcon}/>
         </IconButton>
       </Toolbar>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
     </React.Fragment>
   );
 }
