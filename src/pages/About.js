1aaf8d46b9ae515663fd91088fc426fc3e28044c
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-06-24 18:39:44 +0900

    about 페이지
    main 윗부분 사진 한방에 받아서 넣을거임!

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,7 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -25,20 +26,33 @@ const About = () => {
   const classes = useStyles();
 
   return (
-    <div>
-        <Container component="main1" maxWidth="xl" className={classes.areaIndigo} >
-          <CssBaseline />
-          <div className={classes.paper} >
-              <img src="newsletter_about1.png" width="618.3px" />
+    <div className={classes.areaWhite}>
+      <Container
+        component="subHeader"
+        maxWidth="sm">
+          <div maxWidth="sm">
+            <img src="about_title.png" width="278.1px" ></img>
           </div>
-        </Container>
+          <div className={classes.subtitle} maxWidth="sm">
+            <img src="about_subtitle.png" width="587.1px"></img>
+          </div>
+          
+      </Container>
 
-        <Container component="main2" maxWidth="xl" className={classes.areaWhite} >
-          <CssBaseline />
-          <div className={classes.paper} >
-              <img src="newsletter_about2.png" width="677.5px" />
-          </div>
-        </Container>
+
+      <Container component="main1" maxWidth="xl" className={classes.areaIndigo} >
+        <CssBaseline />
+        <div className={classes.paper} >
+          <img src="newsletter_about1.png" width="618.3px" />
+        </div>
+      </Container>
+
+      <Container component="main2" maxWidth="xl" className={classes.areaWhite} >
+        <CssBaseline />
+        <div className={classes.paper} >
+          <img src="newsletter_about2.png" width="677.5px" />
+        </div>
+      </Container>
     </div>
   );
 }
